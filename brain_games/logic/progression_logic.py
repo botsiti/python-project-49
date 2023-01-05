7cd4d7cb629b94352054702ec5ce95fb74055b14
batraz <batrazbotsiev@gmail.com>
2023-01-05 13:41:38 +0300

    fix progression logic

--- brain_games/logic/progression_logic.py
+++ brain_games/logic/progression_logic.py
@@ -4,24 +4,20 @@ from random import randint, choice
 GAME_MESSAGE = 'What number is missing in the progression?'
 
 
-def prog_settings(first_num):
-    variants = (2, 3, 5)
-    i = choice(variants)
-    prog = []
-    total = randint(7, 10)
-    while total:
-        prog.append(first_num)
-        first_num += i
-        total -= 1
-    return prog
-
-
 def game_settings():
     first_num = randint(1, 50)
-    items = prog_settings(first_num)
-    length = (len(items))
-    remove = randint(0, length - 1)
-    correct_answer = items[remove]
-    items[remove] = '..'
-    question = f'Question: {items}'
+    length = randint(7, 10)
+    variants = (2, 3, 5)
+    iter = choice(variants)
+    prog = []
+    total = length
+    while total:
+        prog.append(first_num)
+        first_num += iter
+        total -= 1
+    dots = randint(0, length)
+    correct_answer = prog[dots]
+    prog[dots] = '..'
+    string = " ".join(map(str, prog[0:10]))
+    question = f'Question: {string}'
     return question, correct_answer
